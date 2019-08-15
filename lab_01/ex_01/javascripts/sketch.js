db75669bc774775983eb190d1ae8a4ce08adb59e
Sayjel Patel <sayjel.v.patel@gmail.com>
2019-08-15 22:41:46 +0400

    updates

--- lab_01/ex_01/javascripts/sketch.js
+++ lab_01/ex_01/javascripts/sketch.js
@@ -1,66 +1,222 @@
 console.log('sketch.js')
 
+let system;
+let interval
+let interval02
+
+function setup() {
+
+  createCanvas(640, 480);
+  system = new ParticleSystem(createVector(50+(width-100)*(minute()/60), hour()/24*(height-100)+50));
+  interval = 0
+  interval02 = 0
+  noStroke()
+  myCallback()
+
+}
+
+function draw() {
+
+  // background('#00000')
+  const date = new Date();
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+ const col = [date.getHours()/24*255,date.getMinutes()/60*255,date.getSeconds()/60*255]
+ background(0)
+ strokeWeight(5)
+  stroke(col)
+  noFill()
+
+  for (var i = 0; i < hours; i++ ){
+    const x = i/24*(width-100)+50 
+    ellipse(x,50,60,60)
+  }
+
+  for (var i = 0; i < minutes; i++ ){
+    
+    const x = i/60*(width-100)+50
+    ellipse(x,100,25,25)
+  
+  }
+
+  for (var i = 0; i < seconds; i++ ){
+    
+    const x = i/60*(width-100)+50 
+    line(x,height/2-50,x,height/2+50)
+
+  }
+  document.getElementById('date').innerHTML = date.toTimeString();
+
+  if (interval > 32){
+    interval = 0
+    myCallback()
+  } else { 
+    interval ++ 
+  }
+
+
+  system.run();
+
+}
+
+
+function myCallback() {
 
 
 
+    const date = new Date();
+ // my Callback 
+  // const date = new Date()
+  const rad = map(date.getSeconds(),0,60,1,30)
+ const col = [date.getHours()/24*255,date.getMinutes()/60*255,date.getSeconds()/60*255]
+  system.addParticle({color:col,radius:rad,date:date});
+  // var intervalID = window.setInterval(myCallback, 1000);
+
+}
+
+// A simple Particle class
+let Particle = function(params) {
+  this.velocity = createVector(random(-0.5, 0.5), random(-0.5,0.5));
+  this.position = params.pos.copy();
+  this.date = params.date.toTimeString()
+
+  this.acceleration = createVector(mouseX-this.position.x, mouseY - this.position.y);
+  this.acceleration.setMag(0.01)
+  this.lifespan = 255;
+  this.color = params.col
+  this.radius = params.radius
+};
+
+Particle.prototype.run = function() {
+  this.update();
+  this.display();
+};
+
+// Method to update position
+Particle.prototype.update = function(){
+  // if (this.position.y > height){
+  //   this.position.y = 0
+  //   return
+  // } 
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.lifespan -= 2;
+};
+
+// Method to display
+Particle.prototype.display = function() {
+  const col = this.color
+  // const c = [...col,this.lifespan]
+  // console.log(col)
+  const t = this.lifespan/10
+
+  fill(col[0],col[1],col[2])
+  // fill(127, this.lifespan);
+  // text(this.date, this.position.x, this.position.y);
+  ellipse(this.position.x, this.position.y, this.radius, this.radius);
+};
+
+// Is the particle still useful?
+Particle.prototype.isDead = function(){
+  return this.lifespan < 0;
+};
+
+let ParticleSystem = function(position) {
+  this.origin = position.copy();
+  this.particles = [];
+};
+
+ParticleSystem.prototype.addParticle = function(params) {
+  this.particles.push(new Particle({pos:this.origin,col:params.color,radius:params.radius,date:params.date}));
+};
+
+ParticleSystem.prototype.run = function() {
+
+  for (let i = this.particles.length-1; i >= 0; i--) {
+    let p = this.particles[i];
+
+    if (i>1){
+      console.log('line')
+
+      // let k = i - 1 
+      // let p02 = this.particles[k]
+      // const col = p.color
+      // const t = p.lifespan/10
+      // stroke(col[0],col[1],col[2],t)
+      // strokeWeight(1)
+      // line(p.position.x,p.position.y,p02.position.x,p02.position.y)
+      // noStroke()
+    
+    }
+
+
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
+    }
+  }
+};
+
+
+// function 
 
 
 // Sketch One
 
-var s = function(q){
+// var s = function(q){
 
-  let canvas
+//   let canvas
 
-  // addapted from https://p5js.org/examples/interaction-tickle.html
+//   // addapted from https://p5js.org/examples/interaction-tickle.html
 
-  let message = 'Tangible Computing'
-  let font;
-  let bounds; // holds x, y, w, h of the text's bounding box
-  let fontsize = 60;
-  let x;
-  let y; // x and y coordinates of the text
+//   let message = 'Tangible Computing'
+//   let font;
+//   let bounds; // holds x, y, w, h of the text's bounding box
+//   let fontsize = 60;
+//   let x;
+//   let y; // x and y coordinates of the text
 
-  q.setup = function() {
+//   q.setup = function() {
 
 
-    q.createCanvas(innerWidth,500)
-    q.background(255)
-    q.textSize(fontsize)
-    q.textAlign(q.CENTER,q.CENTER);
+//     q.createCanvas(innerWidth,500)
+//     q.background(255)
+//     q.textSize(fontsize)
+//     q.textAlign(q.CENTER,q.CENTER);
 
-    // font = q.loadFont('assets/29LTAdir-Medium.ttf')
+//     // font = q.loadFont('assets/29LTAdir-Medium.ttf')
 
-    x = innerWidth / 2 -500
-    y = 500 / 2 
+//     x = innerWidth / 2 -500
+//     y = 500 / 2 
 
-    bounds = {}
-    console.log(x,y)
-    bounds.x = 200
-    bounds.y = 200
+//     bounds = {}
+//     console.log(x,y)
+//     bounds.x = 200
+//     bounds.y = 200
 
-  }
+//   }
 
-  q.draw = function(){
-  	console.log('hi')
+//   q.draw = function(){
+//   	console.log('hi')
 
-    let x = q.mouseX
-    let y = q.mouseY
-    let string = message + ' (' + Math.floor(x) + "," + Math.floor(y) + ')'
+//     let x = q.mouseX
+//     let y = q.mouseY
+//     let string = message + ' (' + Math.floor(x) + "," + Math.floor(y) + ')'
 
-    q.fill(0,10);
-    q.stroke(0)
-    q.text(string, x, y);
-    q.fill(255,50)
-    q.noStroke()
-    q.rect(0,0,q.width,q.height)
-    const date = new Date()
-    document.getElementById('date').innerHTML = date.toTimeString();
+//     q.fill(0,10);
+//     q.stroke(0)
+//     q.text(string, x, y);
+//     q.fill(255,50)
+//     q.noStroke()
+//     q.rect(0,0,q.width,q.height)
 
-  }
 
-}
-console.log('set p5')
-var myp5 = new p5(s, 'clock');
+//   }
+
+// }
+// console.log('set p5')
+// var myp5 = new p5(s, 'clock');
  
 
-    
+//     
---
+++ lab_01/ex_02/solving_sol.js
@@ -0,0 +1,72 @@
+
+Wall Drawing #16 (1969)
+Bands of lines 12 inches (30 cm) wide, in three directions (vertical, horizontal, diagonal right) intersecting.
+
+Doolittle
+Wall Drawing #17 (1969)
+
+Four-part drawing with a different line direction in each part.
+
+y-a-v-a
+Wall Drawing #46 (1970)
+
+Vertical lines, not straight, not touching, covering the wall evenly.
+
+ustina
+Wall Drawing #56 (1970)
+
+A square is divided horizontally and vertically into four equal parts, each with lines in four directions superimposed progressively.
+
+y-a-v-a
+Wall Drawing #87 (1971)
+
+A square divided horizontally and vertically into four equal parts, each with lines and colors in four directions superimposed progressively.
+
+y-a-v-a
+Wall Drawing #91 (1971)
+
+A six-inch (15 cm) grid covering the wall. Within each square, not straight lines from side to side, using red, yellow and blue pencils. Each square contains at least one line of each color.
+
+Doolittle
+Wall Drawing #97 (1971)
+
+Within an 80 inch (200cm) square, 10,000 straight lines. Next to it is an 80 inch (200 cm) square with 10,000 not straight lines.
+
+Nitriques
+Nitriques (canvas)
+Wall Drawing #118 (1971)
+
+On a wall surface, any continuous stretch of wall, using a hard pencil, place fifty points at random. The points should be evenly distributed over the area of the wall. All of the points should be connected by straight lines.
+
+amiccolis
+Wall Drawing #130 (1972)
+
+Grid and arcs from four corners.
+
+Nitriques
+Wall Drawing #273 (1975)
+
+Lines to points on a grid. A six-inch (15 cm) grid covering the wall. Lines from the corners, sides, and center of the walls to random points on the grid. Composite (seventh wall): red lines from the midpoints of four sides, blue lines from four corners, yellow lines from the center.
+
+matthewlein
+Wall Drawing #295 (1976)
+
+Six white geometric figures (outlines) superimposed on a black wall.
+
+Nitriques
+Wall Drawing #328 (1980)
+
+On a black wall, a white circle within which are white vertical parallel lines, and a white parallelogram within which are white horizontal parallel lines. The vertical lines within the circle do not enter the parallelogram, and the horizontal lines within the parallelogram do not enter the circle.
+
+amiccolis
+Wall Drawing #391 (1983)
+
+Two-part drawing. The two walls are each divided horizontally and vertically into four equal parts. First wall: 12-inch (30 cm) bands of lines in four directions, one direction in each part, drawn in black India ink. Second wall: Same, but with four colors drawn in India ink and color ink washes.
+
+amiccolis
+Wall Drawing #797 (1995)
+
+The first drafter has a black marker and makes an irregular horizontal line near the top of the wall. Then the second drafter tries to copy it (without touching it) using a red marker. The third drafter does the same, using a yellow marker. The fourth drafter does the same using a blue marker. Then the second drafter followed by the third and fourth copies the last line drawn until the bottom of the wall is reached.
+
+Doolittle
+Solving Sol is a Whole Pixel project by Brad Bouse.
